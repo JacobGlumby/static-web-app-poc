72e6304e42fa2a3586e6cc28c09cf41692bd68eb
Jacob G Sørensen <jacob.sorensen@twoday.com>
2024-08-06 10:23:17 +0200

    local proxy

--- api/testFunction.cs
+++ api/testFunction.cs
@@ -14,7 +14,7 @@ namespace Company.Function
     {
         [FunctionName("testFunction")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -29,7 +29,7 @@ namespace Company.Function
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(JsonConvert.SerializeObject(responseMessage));
         }
     }
 }
